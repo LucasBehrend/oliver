144556d76d02d881a39c5f82b9f041af9b232f61
LucasBehrend <lucasbehrend2006@gmail.com>
2024-10-02 10:27:47 -0300

    arreglar endpoints turnos

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import { createClient } from '@supabase/supabase-js';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 const supabaseUrl = 'https://rujclvrxksqnijyqcwgz.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1amNsdnJ4a3NxbmlqeXFjd2d6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzgyNjk3MCwiZXhwIjoyMDQzNDAyOTcwfQ.-MBORoWRddYlYuyIbFJKjFFJv2aN8AaHXfiDMsoPCKg";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
+const port = 3000;
 app.use(express.json());
 
+function authenticateToken (req, res, next) {
+    //Bearer token
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    let token_limpio = token.slice(1, -1);
+ 
+    if (token_limpio === null) return res.sendStatus(401);
+    jwt.verify(token_limpio, process.env.ACCESS_TOKEN_SECRET, (err,id) => {
+        if (err) return res.status(403); //token expiration
+        req.id = id;
+        next();
+    })
+}
 async function insertToSupabase(table, values) {
     const error = await supabase
         .from(table)
@@ -16,23 +32,25 @@ async function insertToSupabase(table, values) {
 app.get("/", async (req,res) =>{
     res.send("hola vercel");
 })
-app.post("/turnos", async (req, res) =>{
+app.post("/turnos",  async (req, res) =>{
     const body = req.body;
     const fecha = body.fecha;
     const hora = body.hora;
     const idUsuario = body.idUsuario;
-    const insert_error = insertToSupabase("turnos", {
+    const insert_error = await insertToSupabase("turnos", {
         fecha: fecha,
         hora: hora,
         id_usuario: idUsuario 
     });
-    if (error_insert) {
+    if (insert_error.error) {
+        console.log(insert_error);
         return res.status(500).send('Error inserting data');
     }
+    res.send("turno creado exitosamente");
 } )
 app.get("/turnos", async (req,res) =>{
     const { data, error } = await supabase
-        .from('Estudios')
+        .from('turnos')
         .select('*');
     if (error) {
         console.error('Error fetching data:', error.message);
@@ -41,6 +59,32 @@ app.get("/turnos", async (req,res) =>{
     res.send(data);
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.post("/login", authenticateToken, async (req,res) =>{
+
+    const body = req.body;
+    const name = body.name;
+    const password = body.password;
+
+    const { data, error } = await supabase
+        .from('Usuarios')
+        .select()
+        .eq('nombre_usuarios', body.name);
+    
+    if(!data[0]){
+        res.status(500).send('User not found');    }
+    let compared = await bcrypt.compareSync(password, data[0].password_usuarios);
+    if (compared){
+        const id = data[0].id_usuarios;
+        const accessToken = jwt.sign({id: id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+        res.json(accessToken);
+    }
+    else{
+        res.send("Password incorrect");
+    }
+    if (error) { 
+        res.status(500).send('Error inserting data');
+    }
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/`);
 });
